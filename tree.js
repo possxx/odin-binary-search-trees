b00c8ce323e3567fd7fc5456103871ae2b288167
possxx <168581964+possxx@users.noreply.github.com>
2024-10-18 19:56:11 +0200

    Add buildTree function

--- tree.js
+++ tree.js
@@ -3,7 +3,7 @@ import Node from "./node.js";
 export default class Tree {
   constructor(arr) {
     this.inputArr = this.removeDuplicates(this.mergeSort(arr));
-    this.root = this.buildTree(inputArr);
+    this.root = this.buildTree(this.inputArr);
   }
 
   mergeSort(arr) {
@@ -42,7 +42,38 @@ export default class Tree {
     return newArr;
   }
 
-  buildTree(arr) {}
+  buildTree(arr) {
+    if (arr.length <= 0) return null;
+
+    let mid = Math.floor(arr.length / 2);
+    let data = arr[mid];
+    let leftChild = arr.slice(0, mid - 1);
+    let rightChild = arr.slice(mid + 1);
+
+    let root = new Node();
+    root.data = data;
+    root.leftChild = this.buildTree(leftChild);
+    root.rightChild = this.buildTree(rightChild);
+
+    return root;
+  }
 }
 
-const tree = new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
+const tree = new Tree([
+  1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324, 18, 29, 32, 654, 65, 2,
+]);
+
+const prettyPrint = (node, prefix = "", isLeft = true) => {
+  if (node === null) {
+    return;
+  }
+  if (node.rightChild !== null) {
+    prettyPrint(node.rightChild, `${prefix}${isLeft ? "│   " : "    "}`, false);
+  }
+  console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+  if (node.leftChild !== null) {
+    prettyPrint(node.leftChild, `${prefix}${isLeft ? "    " : "│   "}`, true);
+  }
+};
+
+console.log(tree.root);
