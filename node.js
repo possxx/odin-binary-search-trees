e233741a470b0df4100599b115deee7d9c8b41fc
possxx <168581964+possxx@users.noreply.github.com>
2024-10-23 19:56:45 +0200

    Add data parameter

--- node.js
+++ node.js
@@ -1,7 +1,7 @@
 export default class Node {
-  constructor() {
-    this.data;
-    this.leftChild;
-    this.rightChild;
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
   }
 }
